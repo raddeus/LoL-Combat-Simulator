891a10f1a17526ed3ec2498c8c55448640f519e6
Thad Blankenship <thadblankenship@gmail.com>
2011-10-07 04:27:03 -0500

    Cleaned up code

--- LolComatSimulator/src/combatSim/MainWindow.java
+++ LolComatSimulator/src/combatSim/MainWindow.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
-
 public class MainWindow extends JFrame {
 
 	StatEntryPane leftPane = new StatEntryPane();
@@ -20,17 +19,14 @@ public class MainWindow extends JFrame {
 	Thread FightManagerThread;
 	Champion leftChamp;
 	Champion rightChamp;
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private final JLabel lblDisclaimerThisApp = new JLabel("<html>Disclaimer: This app is in its very early stages of development.<br> There will be bugs and accuracy errors for the time being.<br>If you would like to point an error out to me<br>Please do so on the reddit post<br>or email me at<br>thadblankenship@gmail.com </html>");
-	
+	private final JLabel lblDisclaimerThisApp = new JLabel(
+			"<html>Disclaimer: This app is in its very early stages of development.<br>" +
+			"There will be bugs and accuracy errors for the time being.<br>If you would " +
+			"like to point an error out to me<br>Please do so on the reddit post<br>or email" +
+			" me at<br>thadblankenship@gmail.com </html>");
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -44,58 +40,36 @@ public class MainWindow extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public MainWindow() {
 		setTitle("LoL AD Simulator");
-		
-		
-	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 484, 524);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[182.00][][187.00]", "[grow][][][grow][grow]"));
-		
-		
+		contentPane.setLayout(new MigLayout("", "[182.00][][187.00]",
+				"[grow][][][grow][grow]"));
 		contentPane.add(leftPane, "cell 0 0,grow");
-		
-		
 		contentPane.add(rightPane, "cell 2 0,grow");
-		
 		JButton btnFight = new JButton("FIGHT");
 		btnFight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				leftChamp = leftPane.getChampion();
 				rightChamp = rightPane.getChampion();
-				
-System.out.println("LeftChamp has "+leftChamp.getHealth()+" hp");
-System.out.println("RightChamp has "+rightChamp.getHealth()+" hp");
+				System.out.println("LeftChamp has " + leftChamp.getHealth()
+						+ " hp");
+				System.out.println("RightChamp has " + rightChamp.getHealth()
+						+ " hp");
 				FightManager = new FightManager(leftPane, rightPane);
 				FightManager.addLeftChampion(leftChamp);
 				FightManager.addRightChampion(rightChamp);
 				FightManagerThread = new Thread(FightManager);
-				//Make the statentrypane objects do some shit.
-//				FightManager.addLeftChampion(leftChamp);
-//				FightManager.addRightChampion(rightChamp);
 				FightManager.initFight();
 				FightManagerThread.start();
 			}
 		});
 		contentPane.add(btnFight, "cell 1 2");
-		
 		contentPane.add(lblDisclaimerThisApp, "cell 0 4 3 1");
-
 	}
 
-
-
-
-	}
-	
-	
-
-
-
+}
--- LolComatSimulator/src/combatSim/StatEntryPane.java
+++ LolComatSimulator/src/combatSim/StatEntryPane.java
@@ -8,9 +8,6 @@ import javax.swing.JProgressBar;
 import java.awt.Color;
 
 public class StatEntryPane extends JPanel {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField txtAttackDamage;
 	private JTextField txtAttackSpeed;
@@ -44,73 +41,47 @@ public class StatEntryPane extends JPanel {
 	private final JLabel lblCritDamage = new JLabel("CritDamage%");
 	private final JTextField txtCritDamage = new JTextField();
 	private final JLabel lblHeal = new JLabel(" ");
-	
-	
-	/**
-	 * Create the panel.
-	 */
+
 	public StatEntryPane() {
 		setLayout(new MigLayout("", "[grow][55.00,grow]", "[][][][][][][][][][][][][]"));
-		
-		
 		add(lblAttackDamage, "cell 0 0,alignx trailing");
-		
 		txtAttackDamage = new JTextField();
 		add(txtAttackDamage, "cell 1 0,growx");
 		txtAttackDamage.setColumns(10);
-		
 		add(lblAttackSpeed, "cell 0 1,alignx trailing");
-		
 		txtAttackSpeed = new JTextField();
 		txtAttackSpeed.setColumns(10);
 		add(txtAttackSpeed, "cell 1 1,growx");
-		
 		add(lblCritStrike, "cell 0 2,alignx trailing");
-		
 		txtCriticalStrike = new JTextField();
 		txtCriticalStrike.setColumns(10);
 		add(txtCriticalStrike, "cell 1 2,growx");
-		
 		add(lblLifeSteal, "cell 0 3,alignx trailing");
-		
 		txtLifeSteal = new JTextField();
 		txtLifeSteal.setColumns(10);
 		add(txtLifeSteal, "cell 1 3,growx");
-		
 		add(lblHealth, "cell 0 4,alignx trailing");
-		
 		txtHealth = new JTextField();
 		txtHealth.setColumns(10);
 		add(txtHealth, "cell 1 4,growx");
-		
 		add(lblArmor, "cell 0 5,alignx trailing");
-		
 		txtArmor = new JTextField();
 		txtArmor.setColumns(10);
 		add(txtArmor, "cell 1 5,growx");
-		
 		add(lblDodge, "cell 0 6,alignx trailing");
 		txtDodge.setText("0");
 		txtDodge.setColumns(10);
-		
 		add(txtDodge, "cell 1 6,growx");
-		
 		add(lblCritDamage, "cell 0 7,alignx trailing");
 		txtCritDamage.setText("100");
 		txtCritDamage.setColumns(10);
-		
 		add(txtCritDamage, "cell 1 7,growx");
-		
 		add(lblLiveOutput, "cell 0 9 2 1,alignx center");
-		
 		add(lblCurrentHealth, "cell 0 10 2 1,alignx center");
 		pbCurrentHealth.setToolTipText("Current HP");
-		
 		add(pbCurrentHealth, "cell 0 11 2 1,alignx center");
 		lblHeal.setForeground(Color.GREEN);
-		
 		add(lblHeal, "cell 0 12 2 1,alignx center");
-
 	}
 	
 	public void initPbar(int health){
@@ -127,7 +98,6 @@ public class StatEntryPane extends JPanel {
 		lblCurrentHealth.setText(" "+health+"/"+maxHealth);
 	}
 	
-
 	public Champion getChampion(){
 	armor = Double.parseDouble(txtArmor.getText());
 	attackDamage = Integer.parseInt(txtAttackDamage.getText());
@@ -143,12 +113,10 @@ public class StatEntryPane extends JPanel {
 	}
 
 	public void setLiveOutput(String string) {
-		// TODO Auto-generated method stub
 		lblLiveOutput.setText(string);
-		
 	}
+	
 	public void setHealOutput(double heal){
 		lblHeal.setText("+"+Integer.toString((int)heal));
 	}
-
 }
